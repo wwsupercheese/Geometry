de1fabdc06b4cf54c185ebe2cc3306876de93655
VitalDesctop <levanov2001@bk.ru>
2025-03-06 12:43:41 +0500

    zoom

--- Geometry/FragmentShaderRings.glsl
+++ Geometry/FragmentShaderRings.glsl
@@ -6,8 +6,10 @@ void main() {
     int countRings = 20;
     float rad = length(v_position);
     float color = 1 - int(rad * (countRings + 1)) % 2;
-    vec3 p = vec3(color);
-    o_color = vec4(p, 1.0);
+    vec3 col = vec3(
+        0.5 + 0.5 * sin(color * 10.0),
+        0.5 + 0.5 * sin(color * 15.0 + 1.0),
+        0.5 + 0.5 * sin(color * 20.0 + 2.0)
+    );
+    o_color = vec4(col, 1.0);
 }
-
-
--- Geometry/VertexShader.glsl
+++ Geometry/VertexShader.glsl
@@ -1,10 +1,15 @@
 #version 330
 layout(location = 0) in vec2 a_position;
 layout(location = 1) in vec3 a_color;
+uniform vec2 uOffset;
+uniform float uScale;
 out vec3 v_color;
 out vec2 v_position;
+
 void main() {
+    vec2 scaledPos = a_position * uScale; // NEW
+
+    gl_Position = vec4((a_position * uScale) + vec2(uOffset.x, uOffset.y), 0.0, 1.0);
     v_color = a_color;
     v_position = a_position;
-    gl_Position = vec4(a_position, 0.0, 1.0);
 }
